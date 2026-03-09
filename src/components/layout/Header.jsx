4b0b404003f611a8610bec0c9a00357cef48ddb1
Your Name <jetendrasinghratore1@gmail.com>
2026-03-09 20:15:26 +0530

    new commit

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -89,7 +89,7 @@ export default function BostonHeader() {
                             width={160}
                             height={160}
                             priority
-                            className="h-[120px] w-auto shrink-0"
+                            className="h-[90px] w-auto shrink-0"
                         />
                         <span className="text-white text-base sm:text-lg md:text-xl font-black tracking-[1px] sm:tracking-[2px] md:tracking-[3px] uppercase select-none leading-tight">
                             Bed Bugs Boston
